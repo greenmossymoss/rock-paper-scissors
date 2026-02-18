b2b014660e91bc5c73a0443cca6b7248e9ced9ae
greenmossymoss <mjaubauhellothere@gmail.com>
2026-02-18 22:11:48 +0100

    make buttons work
    script needs to defer
    buttons work now!

--- javascript.js
+++ javascript.js
@@ -1,40 +1,44 @@
-function getComputerChoice () {
+let computerChoice = getComputerChoice()
+// let humanChoice = getHumanChoice().toLowerCase()
+
+function getComputerChoice() {
     num = Math.floor(Math.random() * 3)
     switch (num) {
         case 0:
             return "rock";
-            
+
         case 1:
             return "paper";
-            
+
         case 2:
-            return "scissors";           
-    } 
+            return "scissors";
+    }
 }
 
-function getHumanChoice() {
-    message = 'Rock, paper or scissors?';
-    userInput = prompt(message);
-    return userInput;
-}
+// function getHumanChoice() {
+//     message = 'Rock, paper or scissors?';
+//     userInput = prompt(message);
+//     return userInput;
+// }
 
 let humanScore = 0
 let computerScore = 0
 
-function playRound (computerChoice, humanChoice) {
 
-    computerChoice = getComputerChoice()
-    humanChoice = getHumanChoice().toLowerCase()
 
-    console.log(computerChoice, humanChoice) 
+function playRound(computerChoice, humanChoice) {
 
-    if (computerChoice === humanChoice){
+    
+
+    console.log(computerChoice, humanChoice)
+
+    if (computerChoice === humanChoice) {
         console.log('This round is a tie')
     }
-    
-    else if (computerChoice === 'rock' && humanChoice === 'paper'||
-                computerChoice === 'scissors' && humanChoice === 'rock' ||
-                computerChoice === 'paper' && humanChoice === 'scissors'){
+
+    else if (computerChoice === 'rock' && humanChoice === 'paper' ||
+        computerChoice === 'scissors' && humanChoice === 'rock' ||
+        computerChoice === 'paper' && humanChoice === 'scissors') {
         humanScore += 1
         console.log('This round is a win for you!')
     }
@@ -45,24 +49,61 @@ function playRound (computerChoice, humanChoice) {
 
 }
 
-function playGame () {
+function playGame() {
 
-    playRound()
-    playRound()
-    playRound()
-    
+
+
     console.log(computerScore, humanScore)
 
     if (computerScore > humanScore) {
-        console.log( 'You lost the game :(')
+        console.log('You lost the game :(')
     }
     else if (computerScore < humanScore) {
-        console.log ('Congratulations, you won the game!') 
+        console.log('Congratulations, you won the game!')
     }
     else {
-        console.log('It\'s a tie.') 
-    } 
+        console.log('It\'s a tie.')
+    }
 
 }
 
 playGame()
+
+const wrapper = document.querySelector('#wrapper');
+    wrapper.addEventListener('click', (event) => {
+        let target = event.target;
+
+        switch(target.id) {
+            case 'rock':
+                alert('rock')
+                break;
+            case 'scissors':
+                alert('scissors')
+                break;
+            case 'paper':
+                alert('paper')
+                break;
+        }
+
+    })
+
+// const btn = document.querySelector('button')
+// // const btnScissors = document.querySelector('#scissors')
+// // const btnPaper = document.querySelector('#paper')
+
+// btn.addEventListener('click', () => {
+//     alert('you pressed rock!')})
+
+// const buttons = document.querySelectorAll('button');
+
+// buttons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         alert(button.id);
+//     })
+// })
+
+
+
+
+
+
